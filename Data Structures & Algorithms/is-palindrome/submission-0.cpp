51b13a7da858a65132eb36ba27f435c56782a3b1
satyamjha-dev <151538180+satyamjha-dev@users.noreply.github.com>
2026-06-06 22:37:39 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+
+    int right = s.length() - 1;
+
+    while (left < right) {
+
+        // Skip non-alphanumeric characters from left
+
+        while (left < right && !isalnum(s[left])) {
+
+            left++;
+
+        }
+
+        // Skip non-alphanumeric characters from right
+
+        while (left < right && !isalnum(s[right])) {
+
+            right--;
+
+        }
+
+        // Compare characters (case-insensitive)
+
+        if (tolower(s[left]) != tolower(s[right])) {
+
+            return false;
+
+        }
+
+        left++;
+
+        right--;
+
+    }
+
+    return true;
+        
+    }
+};
